90ad7684a23c68c727c274f50524f64ed8998525
John Kiernander <jkiernander@pmsi-consulting.com>
2013-08-16 15:50:46 +0100

    Fix Tooltip

--- dist/dimple.v1.js
+++ dist/dimple.v1.js
@@ -2355,7 +2355,7 @@ var dimple = {
                 }, this);
             } else {
                 // Add the axis measure value
-                rows.push(series.x.measure + ": " + series.x._getFormat()(e.x));
+                rows.push(series.x.measure + ": " + series.x._getFormat()((e.cx === e.x ? e.xValue : e.cx - e.x)));
             }
 
             if (series.y._hasTimeField()) {
@@ -2367,7 +2367,7 @@ var dimple = {
                 }, this);
             } else {
                 // Add the axis measure value
-                rows.push(series.y.measure + ": " + series.y._getFormat()(e.y));
+                rows.push(series.y.measure + ": " + series.y._getFormat()((e.cy === e.y ? e.yValue : e.cy - e.y)));
             }
 
             if (series.z !== null && series.z !== undefined) {
