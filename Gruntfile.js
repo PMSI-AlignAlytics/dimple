3679cf74fc748d8745d1117f8027c6bd35d33e25
John Kiernander <jkiernander@pmsi-consulting.com>
2013-09-12 10:17:06 +0100

    Introduce version handling

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+    "use strict";
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        prop: {
+            dist: {
+                src: [
+                    'templates/*.html'
+                ]
+            },
+            options: {
+                dest: '',
+                tag: '{version}',
+                version: 'v<%= pkg.version %>',
+                header: "<!--------------------------------------------------------------------->\n" +
+                        "<!-- AUTOMATICALLY GENERATED CODE - PLEASE EDIT {sourcefile} INSTEAD -->\n" +
+                        "<!--------------------------------------------------------------------->\n"
+            }
+        }
+    });
+
+    // Propogate version into relevant files
+    grunt.registerMultiTask('prop', 'Propagate Versions.', function() {
+        var options = this.options(),
+            outPath = options.dest,
+            tag = options.tag,
+            version = options.version,
+            header = options.header;
+        grunt.log.writeln("");
+        grunt.log.writeln("Replacing " + tag + " with " + version);
+        grunt.log.writeln("------------------------------------------------------");
+        this.files.forEach(function(f) {
+            f.src.filter(function(filepath) {
+                var result = true;
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('File "' + filepath + '" not found.');
+                    result = false;
+                }
+                return result;
+            }).map(function(filepath) {
+                // Read file source.
+                var src = grunt.file.read(filepath),
+                    head = header.split("{sourcefile}").join(filepath);
+                // Replace the version
+                src = src.split(tag).join(version);
+                // Write the new file
+                grunt.log.writeln(filepath + " -> " + outPath + filepath.substring(filepath.lastIndexOf("/") + 1));
+                grunt.file.write(outPath + filepath.substring(filepath.lastIndexOf("/") + 1), head + src);
+            });
+        });
+    });
+
+    // Default tasks
+    grunt.registerTask('default', ['prop']);
+
+};
