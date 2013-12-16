793446c795e94e615cefb56790cec2bbad7aaa0a
John Kiernander <jkiernander@pmsi-consulting.com>
2013-12-16 10:31:34 +0000

    Update to 1.1.3

--- src/objects/chart/methods/defaultColors.js
+++ src/objects/chart/methods/defaultColors.js
@@ -3,16 +3,16 @@
         // Source: /src/objects/chart/methods/defaultColors.js
         // Help: http://github.com/PMSI-AlignAlytics/dimple/wiki/dimple.chart#wiki-defaultColors
         this.defaultColors = [
-            new dimple.color("#80B1D3"),
-            new dimple.color("#FB8072"),
-            new dimple.color("#FDB462"),
-            new dimple.color("#B3DE69"),
-            new dimple.color("#FFED6F"),
-            new dimple.color("#BC80BD"),
-            new dimple.color("#8DD3C7"),
-            new dimple.color("#CCEBC5"),
-            new dimple.color("#FFFFB3"),
-            new dimple.color("#BEBADA"),
-            new dimple.color("#FCCDE5"),
-            new dimple.color("#D9D9D9")
+            new dimple.color("#80B1D3"), // Blue
+            new dimple.color("#FB8072"), // Red
+            new dimple.color("#FDB462"), // Orange
+            new dimple.color("#B3DE69"), // Green
+            new dimple.color("#FFED6F"), // Yellow
+            new dimple.color("#BC80BD"), // Purple
+            new dimple.color("#8DD3C7"), // Turquoise
+            new dimple.color("#CCEBC5"), // Pale Blue
+            new dimple.color("#FFFFB3"), // Pale Yellow
+            new dimple.color("#BEBADA"), // Lavender
+            new dimple.color("#FCCDE5"), // Pink
+            new dimple.color("#D9D9D9")  // Grey
         ];
