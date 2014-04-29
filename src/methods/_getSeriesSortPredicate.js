da98ddff2cbc82f6800e19587f0f009a6ac92fa9
johnkiernander <jkiernander@pmsi-consulting.com>
2014-04-29 10:04:17 +0100

    Fix Issue #71

--- src/methods/_getSeriesSortPredicate.js
+++ src/methods/_getSeriesSortPredicate.js
@@ -4,11 +4,15 @@
     dimple._getSeriesSortPredicate = function (chart, series, orderedArray) {
         return function (a, b) {
             var sortValue = 0;
+            // Modified to keep trying until a difference is found.  Previously these were else if statements
+            // Issue #71
             if (series.x._hasCategories()) {
-                sortValue = (dimple._helpers.cx(a, chart, series) < dimple._helpers.cx(b, chart, series) ? -1 : 1);
-            } else if (series.y._hasCategories()) {
-                sortValue = (dimple._helpers.cy(a, chart, series) < dimple._helpers.cy(b, chart, series) ? -1 : 1);
-            } else if (orderedArray) {
+                sortValue = (dimple._helpers.cx(a, chart, series) - dimple._helpers.cx(b, chart, series));
+            }
+            if (sortValue === 0 && series.y._hasCategories()) {
+                sortValue = (dimple._helpers.cy(a, chart, series) - dimple._helpers.cy(b, chart, series));
+            }
+            if (sortValue === 0 && orderedArray) {
                 sortValue = dimple._arrayIndexCompare(orderedArray, a.aggField, b.aggField);
             }
             return sortValue;
